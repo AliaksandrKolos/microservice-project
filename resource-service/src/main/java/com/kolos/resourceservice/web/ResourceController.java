a1fe8cdc6b5e6ead700ee7da9d4ec58c03290336
sanya <kolosovskii.201416@gmail.com>
2024-10-17 18:36:29 +0300

    Implemented a ResourceController controller with upload, download and delete methods

---
+++ resource-service/src/main/java/com/kolos/resourceservice/web/ResourceController.java
@@ -0,0 +1,36 @@
+package com.kolos.resourceservice.web;
+
+import com.kolos.resourceservice.service.ResourceService;
+import com.kolos.resourceservice.service.dto.ResourceIdDto;
+import com.kolos.resourceservice.service.dto.ResourceIdsDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/resources")
+public class ResourceController {
+
+    private final ResourceService resourceService;
+
+
+    @PostMapping
+    public ResourceIdDto upload(@RequestParam("file") MultipartFile file) throws IOException {
+        return resourceService.upload(file);
+    }
+
+
+    @GetMapping("/{id}")
+    public byte[] download(@PathVariable long id) {
+        return resourceService.download(id);
+    }
+
+    @DeleteMapping
+    public ResourceIdsDto delete(@RequestParam("id") List<Long> ids) {
+        return resourceService.delete(ids);
+    }
+}
