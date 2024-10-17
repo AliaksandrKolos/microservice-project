dcb1362649084d8b59879fa1adebe7d8dff88c7e
sanya <kolosovskii.201416@gmail.com>
2024-10-17 18:54:03 +0300

    Created the SongClient interface

---
+++ resource-service/src/main/java/com/kolos/resourceservice/client/SongClient.java
@@ -0,0 +1,12 @@
+package com.kolos.resourceservice.client;
+
+import com.kolos.resourceservice.service.dto.MetaDataDto;
+
+import java.util.List;
+
+public interface SongClient {
+
+    void create(MetaDataDto metaDataDto);
+
+    void deleteByResourceId(List<Long> resourceId);
+}
