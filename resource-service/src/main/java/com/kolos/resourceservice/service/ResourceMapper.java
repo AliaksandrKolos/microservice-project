f60ac7d2a574909b42ba16d79525dc05b490e6e9
sanya <kolosovskii.201416@gmail.com>
2024-10-27 16:47:44 +0300

    Added  new methods to ResourceMapper: toDtoResource

--- resource-service/src/main/java/com/kolos/resourceservice/service/ResourceMapper.java
+++ resource-service/src/main/java/com/kolos/resourceservice/service/ResourceMapper.java
@@ -1,6 +1,7 @@
 package com.kolos.resourceservice.service;
 
 import com.kolos.resourceservice.data.entity.Resource;
+import com.kolos.resourceservice.service.dto.ResourceDto;
 import com.kolos.resourceservice.service.dto.ResourceIdDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -10,4 +11,6 @@ public interface ResourceMapper {
 
     @Mapping(source = "id", target = "resourceId")
     ResourceIdDto toDto(Resource resource);
+
+    ResourceDto toDtoResource(Resource resource);
 }
