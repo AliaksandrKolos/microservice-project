8957e33711ef4007a92bf9a56560c5b2061534f3
sanya <kolosovskii.201416@gmail.com>
2024-10-30 17:22:15 +0300

    Added error handling for RequestNotPermitted and UnknownHostException

--- resource-service/src/main/java/com/kolos/resourceservice/web/ErrorHandler.java
+++ resource-service/src/main/java/com/kolos/resourceservice/web/ErrorHandler.java
@@ -1,13 +1,16 @@
 package com.kolos.resourceservice.web;
 
 import com.kolos.resourceservice.service.exception.UnsupportedTypeException;
+import io.github.resilience4j.ratelimiter.RequestNotPermitted;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.net.UnknownHostException;
 import java.util.NoSuchElementException;
 
 @Slf4j
@@ -29,8 +32,21 @@ public class ErrorHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(UnsupportedTypeException.class)
     public void badRequest(UnsupportedTypeException e) {
-        log.info("Unsupported media type: " + e.getMessage());
+        log.info("Unsupported media type: {}", e.getMessage());
     }
+
+    @ResponseStatus(HttpStatus.TOO_MANY_REQUESTS)
+    @ExceptionHandler(RequestNotPermitted.class)
+    public String notPermitted(RequestNotPermitted e) {
+        return "Too many requests go home.";
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(UnknownHostException.class)
+    public String hostException(UnknownHostException e) {
+        return "Unknown host: " + e.getMessage();
+    }
+
 }
 
 
