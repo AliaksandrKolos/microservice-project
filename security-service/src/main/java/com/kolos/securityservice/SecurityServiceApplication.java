c88eba8a65d0304737036b79c66b0be1110da6a6
sanya <kolosovskii.201416@gmail.com>
2024-10-31 14:16:24 +0300

    Added security-service service

---
+++ security-service/src/main/java/com/kolos/securityservice/SecurityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kolos.securityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityServiceApplication.class, args);
+    }
+
+}
