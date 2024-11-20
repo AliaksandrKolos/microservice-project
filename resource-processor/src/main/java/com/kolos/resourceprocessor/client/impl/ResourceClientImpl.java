575cf479f75f52409a0fc6a3813a025813cf0164
Alexander Kolosovsky <kolosovskii.201416@gmail.com>
2024-11-20 12:28:09 +0300

    Replaced Component annotation with Service in ResourceClientImpl

--- resource-processor/src/main/java/com/kolos/resourceprocessor/client/impl/ResourceClientImpl.java
+++ resource-processor/src/main/java/com/kolos/resourceprocessor/client/impl/ResourceClientImpl.java
@@ -7,11 +7,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Slf4j
-@Component
+@Service
 @RequiredArgsConstructor
 public class ResourceClientImpl implements ResourceClient {
 
