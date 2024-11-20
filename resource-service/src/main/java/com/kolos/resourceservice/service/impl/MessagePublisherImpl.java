e939321e82d1426eab5853bc718a348c1f9e8711
Alexander Kolosovsky <kolosovskii.201416@gmail.com>
2024-11-20 12:08:02 +0300

    Placed the values in the properties file in MessagePublisherImpl

--- resource-service/src/main/java/com/kolos/resourceservice/service/impl/MessagePublisherImpl.java
+++ resource-service/src/main/java/com/kolos/resourceservice/service/impl/MessagePublisherImpl.java
@@ -4,6 +4,7 @@ import com.kolos.resourceservice.service.MessagePublisher;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -15,21 +16,37 @@ public class MessagePublisherImpl  implements MessagePublisher {
 
     private final RabbitTemplate rabbitTemplate;
 
+    @Value("${routing.key.save.metaData}")
+    private String routingKeySaveMetaData;
+
+    @Value("${routing.key.deleteIds}")
+    private String routingKeyDeleteIds;
+
+    @Value("${routing.key.communication.health.check}")
+    private String routingKeyCommunicationHealthCheck;
+
+    @Value("${exchange.name}")
+    private String nameExchange;
+
+    @Value("${send.object}")
+    private String sendObject;
+
+
     @Override
     public void postId(Long id) {
         log.info("Publishing message {}", id);
-        rabbitTemplate.convertAndSend("save_metadata", id);
+        rabbitTemplate.convertAndSend(routingKeySaveMetaData, id);
     }
 
     @Override
     public void postIds(List<Long> ids) {
         log.info("Publishing message {}", ids);
-        rabbitTemplate.convertAndSend("deleteIds", ids);
+        rabbitTemplate.convertAndSend(routingKeyDeleteIds, ids);
     }
 
     @Override
     public void healthCheck() {
         log.info("Publishing health check");
-        rabbitTemplate.convertAndSend("healthCheckTopic","communicationHealthCheck","Communication Health-Check Event");
+        rabbitTemplate.convertAndSend(nameExchange, routingKeyCommunicationHealthCheck, sendObject);
     }
 }
