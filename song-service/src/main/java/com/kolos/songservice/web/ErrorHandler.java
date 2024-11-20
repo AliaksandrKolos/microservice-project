3d2bc7b7e3b3ef55a5fec10bd39db6c3fe739b45
Alexander Kolosovsky <kolosovskii.201416@gmail.com>
2024-11-20 12:34:10 +0300

    Added 2 custom exceptions and InvalidInputException handling

--- song-service/src/main/java/com/kolos/songservice/web/ErrorHandler.java
+++ song-service/src/main/java/com/kolos/songservice/web/ErrorHandler.java
@@ -1,16 +1,27 @@
 package com.kolos.songservice.web;
 
+import com.kolos.songservice.service.exception.InvalidInputException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.NoSuchElementException;
 
 @Slf4j
 @RestControllerAdvice
 public class ErrorHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<Map<String, String>> invalidInputException(InvalidInputException e) {
+        Map<String, String> errors = new HashMap<>();
+        errors.put("message", e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
