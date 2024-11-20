887e4780a3dbe85586631636ad0b737999612d92
Alexander Kolosovsky <kolosovskii.201416@gmail.com>
2024-11-20 12:35:06 +0300

    put the names in the properties file in RabbitmqConfig

--- song-service/src/main/java/com/kolos/songservice/RabbitmqConfig.java
+++ song-service/src/main/java/com/kolos/songservice/RabbitmqConfig.java
@@ -4,31 +4,46 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitmqConfig {
 
+    @Value("${queue.delete.ids}")
+    private String queueDeleteIds;
+
+    @Value("${queue.health.check}")
+    private String queueHealthCheck;
+
+    @Value("${topic.exchange.health.check}")
+    private String topicHealthCheck;
+
+    @Value("${routing.key.health.check}")
+    private String routingKeyCommunicationHealthCheck;
+
+
+
     @Bean
     public Queue deleteIds() {
-        return new Queue("deleteIds", false);
+        return new Queue(queueDeleteIds, false);
     }
 
     @Bean
     public Queue healthCheckQueue() {
-        return new Queue("health_check_song_service", false);
+        return new Queue(queueHealthCheck, false);
     }
 
     @Bean
     public TopicExchange topicExchange() {
-        return new TopicExchange("healthCheckTopic");
+        return new TopicExchange(topicHealthCheck);
     }
 
     @Bean
     public Binding bindingHealthCheck(Queue healthCheckQueue, TopicExchange topicExchange) {
         return BindingBuilder.bind(healthCheckQueue)
                 .to(topicExchange)
-                .with("communicationHealthCheck");
+                .with(routingKeyCommunicationHealthCheck);
     }
 }
