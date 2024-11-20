630f9ff2a9f67743f520f9f8a94bd95ec049ed04
Alexander Kolosovsky <kolosovskii.201416@gmail.com>
2024-11-20 11:57:09 +0300

    Created an error interceptor class ExceptionController in gateway-service

---
+++ gateway-service/src/main/java/com/kolos/gatewayservice/exception/ExceptionController.java
@@ -0,0 +1,21 @@
+package com.kolos.gatewayservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class ExceptionController {
+
+    @ExceptionHandler
+    public ResponseEntity<Map<String, String>> securityHandler(SecurityException e) {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
+}
