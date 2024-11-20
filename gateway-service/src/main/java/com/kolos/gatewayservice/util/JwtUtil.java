620bccd269d1cdad86d44a5dbe7027d2698bd7b9
Alexander Kolosovsky <kolosovskii.201416@gmail.com>
2024-11-20 11:58:12 +0300

    Replaced the @Service annotation with @Component in the JwtUtil class

--- gateway-service/src/main/java/com/kolos/gatewayservice/util/JwtUtil.java
+++ gateway-service/src/main/java/com/kolos/gatewayservice/util/JwtUtil.java
@@ -5,13 +5,13 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.function.Function;
 
-@Service
+@Component
 public class JwtUtil {
 
     @Value("${security.key}")
